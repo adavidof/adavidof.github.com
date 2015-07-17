8758fde2c27a44d2d584ccd7b8b759f8e79dddfd
adavidof <xdvidof@gmail.com>
2015-07-17 22:25:49 +0300

    init

---
+++ taglist/js/app.js
@@ -0,0 +1,7 @@
+$(function() {
+    'use strict';
+
+    var tagList1 = new TagList('.taglist1', ['First', 'Second', 'Third']);
+    var tagList2 = new TagList('.taglist2', ['Integer', 'Float', 'Boolean', 'null', 'undefined', 'String', 'Function', 'Object', 'Array']);
+    var tagList3 = new TagList('.taglist3');
+});
